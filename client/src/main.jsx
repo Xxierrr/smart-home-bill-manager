c1b3f5b4069ce700bd7e05f9e5b391dfbdffeaa1
Gyan Prakash Tiwari <gyanprakash@example.com>
2026-04-01 14:30:39 +0530

    Add Vercel Analytics for visitor and page view tracking

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './index.css'
 
@@ -13,6 +14,7 @@ const root = ReactDOM.createRoot(rootElement)
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 )
 
